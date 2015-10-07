d69ea074d12ce54434178605ac0a31208cba3784
vvakil <vivek.vakil@yale.edu>
2015-10-07 02:17:20 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
+app.get('/aggressive-locust', function(request, response){
+    response.send('aggressive-locust');
+});
+
 app.listen(process.env.PORT || 4000);
